916e14a630a2c518eed6177c643553e45db623be
Dan Lehman <5494894+vegaZenith@users.noreply.github.com>
2024-09-05 08:55:04 -0400

    add prompt to change grid size Added a a button that, when pressed, issues a prompt to the user and checks that the value is valid. The size of the cells is recalculated so that the total grid occupies the same space.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
+// total width/height that the grid will occupy
+const totalSpace = 540;
+
+const button = document.querySelector("button");
+button.addEventListener("click", makeNewGrid);
+
 createGrid(16);
 
 function createGrid(gridSize){
     const container = document.querySelector(".container");
+    const cellSize = Math.floor(totalSpace / gridSize) - 2;
     for(let i=0; i<gridSize; i++){
         let row = document.createElement("div");
         row.classList.add("row");
@@ -9,6 +16,8 @@ function createGrid(gridSize){
             let cell = document.createElement("div");
             cell.classList.add("cell");
             cell.addEventListener("mouseover", changeColour);
+            cell.style.width = cellSize + "px";
+            cell.style.height = cellSize + "px";
             row.appendChild(cell);
         }
         container.appendChild(row);
@@ -18,4 +27,17 @@ function createGrid(gridSize){
 // callback function for mouseover event listener
 function changeColour(e){
     e.target.classList.add("hovered");
+}
+
+function makeNewGrid(){
+    const container = document.querySelector(".container");
+
+    //remove all children from the container
+    container.textContent = "";
+
+    let gridSize = Number(prompt("Grid Size"));
+    while(isNaN(gridSize) || gridSize < 1 || gridSize > 100){
+        gridSize = prompt("Grid Size");
+    }
+    createGrid(gridSize);
 }
